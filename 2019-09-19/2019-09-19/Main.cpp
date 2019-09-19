7b4a9a04d2d2812f7d7385c719b5db0518b8b53f
MaskedOut <55443481+MaskedOut@users.noreply.github.com>
2019-09-19 08:44:29 -0400

    Add files via upload

---
+++ 2019-09-19/2019-09-19/Main.cpp
@@ -0,0 +1,25 @@
+//  Name: Izaiah Patterson
+// Intro To CS
+// Period 1
+// Mr. Williams
+// 2019-09-19 Homework
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Name: Izaiah Patterson" << std::endl
+		<< "Class Schedule" << std::endl
+		<< "Period 1: Computer Science" << std::endl
+		<< "Period 2: Global History and Geography" << std::endl
+		<< "Period 3: Health" << std::endl
+		<< "Period 4: House" << std::endl
+		<< "Period 5: Spanish" << std::endl
+		<< "Period 6: Lunch" << std::endl
+		<< "Period 7: English" << std::endl
+		<< "Period 8: Earth Science" << std::endl
+		<< "Period 9: Gym" << std::endl
+		<< "Period 10: Geometry" << std::endl;
+
+
+}
